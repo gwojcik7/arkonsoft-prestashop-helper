7336f3e2d1bbbe81b3a867ca61429726ace9c71b
Grzegorz Wójcik <gwojcik@arkonsoft.pl>
2021-04-28 22:43:58 +0200

    init

---
+++ src/Arkonsoft/PrestaShop/Dictionaries/ModuleTabDictionary.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Arkonsoft\PrestaShop\Dictionaries;
+
+class ModuleTabDictionary
+{
+    const ADMINISTRATION = 'administration';
+    const ADVERTISING_MARKETING = 'advertising_marketing';
+    const ANALYTICS_STATS = 'analytics_stats';
+    const BILLING_INVOICING = 'billing_invoicing';
+    const CHECKOUT = 'checkout';
+    const CONTENT_MANAGEMENT = 'content_management';
+    const DASHBOARD = 'dashboard';
+    const EMAILING = 'emailing';
+    const EXPORT = 'export';
+    const FRONT_OFFICE_FEATURES = 'front_office_features';
+    const I18N_LOCALIZATION = 'i18n_localization';
+    const MARKET_PLACE = 'market_place';
+    const MERCHANDIZING = 'merchandizing';
+    const MIGRATION_TOOLS = 'migration_tools';
+    const MOBILE = 'mobile';
+    const OTHERS = 'others';
+    const PAYMENTS_GATEWAYS = 'payments_gateways';
+    const PAYMENT_SECURITY = 'payment_security';
+    const PRICING_PROMOTION = 'pricing_promotion';
+    const QUICK_BULK_UPDATE = 'quick_bulk_update';
+    const SEARCH_FILTER = 'search_filter';
+    const SEO = 'seo';
+    const SHIPPING_LOGISTICS = 'shipping_logistics';
+    const SLIDESHOWS = 'slideshows';
+    const SMART_SHOPPING = 'smart_shopping';
+    const SOCIAL_NETWORKS = 'social_networks';
+}
