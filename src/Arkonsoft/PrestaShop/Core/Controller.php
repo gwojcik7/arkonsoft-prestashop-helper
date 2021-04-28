735fceea0fc333eb1018ed4494f599e42b3d8ed7
Grzegorz Wójcik <gwojcik@arkonsoft.pl>
2021-04-28 23:30:30 +0200

    add contructor to base Controller

--- src/Arkonsoft/PrestaShop/Core/Controller.php
+++ src/Arkonsoft/PrestaShop/Core/Controller.php
@@ -2,6 +2,14 @@
 
 namespace Arkonsoft\Prestashop\Core;
 
+use Module;
+
 abstract class Controller implements IController
 {
+    protected $module;
+
+    public function __construct(Module $module)
+    {
+        $this->module = $module;
+    }
 }
